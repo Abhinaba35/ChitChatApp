f3592ffbcf37cc66c55668f8231bfb49b806bab7
Abhinaba <dasjayanti208@gmail.com>
2025-06-15 17:58:58 +0530

    fix: downgrade express to stable version and clean up code formatting in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route , Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/login/login.jsx";
 import SignUp from "./pages/signup/signup.jsx";
 import Home from "./pages/home/home.jsx";
@@ -6,21 +6,26 @@ import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./context/authContext.jsx";
 
 function App() {
-      const {authUser} = useAuthContext();
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={ authUser ?  <Home />  : <Navigate to = {"/login"} />} />
-        <Route path="/login" element={ authUser ? <Navigate to = "/" /> : <Login />} />
-        <Route path="/signup" element={authUser ? <Navigate to = "/" /> : <SignUp />} />
-        {/* Add more routes as needed */}
-      </Routes>
-
+  const { authUser } = useAuthContext();
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
       <Toaster />
-
-    </>
-  );
+    </>
+  );
 }
 
 export default App;
-
